94a742beeba1553027c4c55d5399ccebbb525251
meetqy <meetqy@icloud.com>
2026-06-11 22:45:26 +0800

    x

--- src/components/global-footer.tsx
+++ src/components/global-footer.tsx
@@ -14,7 +14,6 @@ export const friendLinks: string[] = [
   `<a href="https://startupfa.st" target="_blank" title="Powered by Startup Fast"><img src="https://startupfa.st/images/badges/powered-by-light.svg" alt="Powered by Startup Fast" width="150" height="44"/></a>`,
   `<a href="https://huntifyai.com/tools/makegraph" target="_blank" rel="noopener noreferrer"><img src="https://huntifyai.com/api/badge?theme=light" alt="Featured on HuntifyAI" width="220" height="54" /></a>`,
   `<a href="https://aitoolfame.com/item/makegraph" target="_blank" rel="noopener noreferrer"><img src="https://aitoolfame.com/badge-light.svg" alt="Featured on aitoolfame.com" style="height: 54px; width: auto;" /></a>`,
-  `<a href="https://codetrendy.com" target="_blank" rel="noopener noreferrer"><img src="https://codetrendy.com/api/badge?style=classic" alt="Listed on codetrendy.com" height="54" /></a>`,
   `<a href="https://domainrank.app" target="_blank"><img src="https://domainrank.app/api/badge/makegraph.org" alt="makegraph.org Domain Rating" width="360" height="80" /></a>`,
   `<a href="https://www.direct2app.com" target="_blank" rel="noopener"><img src="https://www.direct2app.com/featured-light.svg" alt="Featured On Direct2App" style="height: 54px; width: auto;" /></a>`,
   `<a href="https://findly.tools/make-graph?utm_source=make-graph" target="_blank" rel="noopener noreferrer"><img src="https://findly.tools/badges/findly-tools-badge-light.svg" alt="Featured on Findly.tools" width="175" height="55" /></a>`,
