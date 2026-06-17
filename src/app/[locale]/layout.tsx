34c1553dfd3a9a6c1a3047d57064550d03455bf5
meetqy <meetqy@icloud.com>
2026-06-17 17:49:58 +0800

    Clarity

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -2,6 +2,7 @@ import '~/styles/globals.css';
 
 import type { Metadata } from 'next';
 import { Geist } from 'next/font/google';
+import Script from 'next/script';
 
 import { GlobalBackground } from '~/components/global-background';
 import { GlobalFooter } from '~/components/global-footer';
@@ -63,6 +64,13 @@ export default async function RootLayout({
     <html className={`${geist.variable}`} lang={locale}>
       <head>
         <meta name="stackscope-claim" content="aZyd5K4q" />
+        <Script
+          id="microsoft-clarity"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `(function(c,l,a,r,i,t,y){c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);})(window, document, "clarity", "script", "x89ea8i48v");`,
+          }}
+        />
       </head>
       <TRPCReactProvider>
         <NextIntlClientProvider messages={messages}>
